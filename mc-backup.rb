4c2ee023181cd63fa7d894b28ed4773ae5434ddf
Xu Cheng <xucheng@me.com>
2016-05-26 02:08:43 +0800

    backup: use mcrcon

--- mc-backup.rb
+++ mc-backup.rb
@@ -12,7 +12,7 @@ class Pathname
 end
 
 def system(*args)
-  puts args.shelljoin
+  puts args.shelljoin.gsub(MCRCON_PW, "<token>")
   super
 end
 
@@ -27,15 +27,19 @@ MC_PATH = Pathname.new "/opt/minecraft/server/"
 BAK_PATH = Pathname.new "/opt/minecraft/backup"
 STAGE_PATH = BAK_PATH/"stage"
 AR_PATH = BAK_PATH/"archives"
+MCRCON = MC_PATH/"mcrcon"
+MCRCON_PW = (MC_PATH/".mcrcon-pass").read.strip
 
 lock "/tmp/mc-backup.lock"
 
 mkdir_p STAGE_PATH
 mkdir_p AR_PATH
 
+system MCRCON, "-H", "localhost", "-p", MCRCON_PW, "say [Server] Backup started."
 system "rsync", "-avcr", "--exclude", "logs", MC_PATH, STAGE_PATH
 cd(STAGE_PATH) do
   system "tar", "-czvf", AR_PATH/"#{Time.now.iso8601}.tar.gz", "."
 end
 system "find", AR_PATH, "-mtime", "+3", "-type", "f", "-print", "-delete"
+system MCRCON, "-H", "localhost", "-p", MCRCON_PW, "say [Server] Backup done."
 
